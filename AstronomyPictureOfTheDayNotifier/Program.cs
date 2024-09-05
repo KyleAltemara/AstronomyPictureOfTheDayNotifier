4e7b5225a656d04c3d2cec8cd6e4d79e0f4dc604
Kyle <Kyle.Altemara@gmail.com>
2024-09-05 16:03:47 -0700

    Can get data from https://apod.nasa.gov/apod/astropix.html

---
+++ AstronomyPictureOfTheDayNotifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using HtmlAgilityPack;
+
+namespace AstronomyPictureOfTheDayNotifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://apod.nasa.gov/apod/astropix.html";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var imageName = doc.DocumentNode.SelectSingleNode("/html/body/center[2]/b[1]").InnerText;
+            Console.WriteLine(imageName);
+        }
+    }
+}
